a175e64d8765d2add8910ec441c8169ba47fa041
jash <javiercamarerolopez@gmail.com>
2024-11-11 10:26:44 +0100

    Build autenticación de datos

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require('express')
 const { exec } = require('child_process')
 const figlet = require('figlet')
+const crypto = require('crypto');
+const { getUser } = require('./database');
 
 const app = express()
 const port = 8000
 
 app.use(express.static("public"))
+
+const realm = 'User Visible Realm';
+
+function authMiddleware(req, res, next) {
+    const authHeader = req.headers['authorization'];
+  
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      // Si no hay cabecera de autorización o no es del tipo Basic, pedir credenciales
+      res.setHeader('WWW-Authenticate', `Basic realm="${realm}"`);
+      return res.status(401).send('Autenticación requerida');
+    }
+  
+    // Decodificar credenciales base64
+    const base64Credentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+  
+    const user = getUser(username);
+    const md5hash = crypto.createHash('md5').update(password).digest('hex');
+  
+    if (!user || user.password !== md5hash) {
+      // Si el usuario no existe o la contraseña es incorrecta
+      res.setHeader('WWW-Authenticate', `Basic realm="${realm}"`);
+      return res.status(401).send('Credenciales incorrectas');
+    }
+  
+    // Si las credenciales son correctas, continuar con la siguiente función
+    return next();
+  }
 
 app.get("/", (req, res) => {
     exec(comando, (error, stdout, stderr) =>{
@@ -23,7 +54,7 @@ app.get("/ping", (req, res) => {
 })
 
 //Obtenemos el texto con figlet
-app.get("/figlet", (req, res) =>{
+app.get("/figlet", authMiddleware, (req, res) =>{
     const texto = req.query.texto
     const fuentes = req.query.fuente
 
---
+++ start.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users (user TEXT PRIMARY KEY, password TEXT)
+INSERT INTO users (user, password) VALUES ('admin', '81dc9bdb52d04dc20036dbd8313ed055')
+INSERT INTO users (user, password) VALUES ('pikachu', '04dac8afe0ca501587bad66f6b5ce5ad')
